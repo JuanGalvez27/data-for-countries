73a30ed74acb4e3ac9a872db7dcb924d7ca8b9c0
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-08-29 10:34:09 -0500

    Functionality done, styles missing

--- src/components/CountryChar.jsx
+++ src/components/CountryChar.jsx
@@ -3,19 +3,17 @@ import { useState, useEffect } from 'react';
 
 const CountryChar = ({ country }) => {
   // const api_key = process.env.REACT_API_KEY;
-  const [weather, setWeather] = useState([])
-  
-  const weatherMap = () => {
+  const [weather, setWeather] = useState(['hola']);
+
+
+
+const localWeather = () => () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${country.latlng[0]}&lon=${country.latlng[1]}&appid=2fd0d5bec943df89ba9d59bb9b035925`)
       .then(res => res.json())
       .then(json => setWeather(json))
-  }
+    }
   // eslint-disable-next-line
-  useEffect(weatherMap, [country.capital[0]] )
-
-  const temperature = (parseFloat(weather.main.temp) - 273.15).toFixed(2)
-  const wind = weather.wind.speed;
-
+  useEffect( localWeather , [country] )
 
   const languages = Object.values(country.languages)
   
@@ -29,7 +27,7 @@ const CountryChar = ({ country }) => {
       <h3>Languages</h3>
       <ul>
         {languages.map(e => (
-          <li>{e}</li>
+          <li key={e}>{e}</li>
         ))}
       </ul>
       <img src={country.flags.png} alt={`${country.name.common} flag`} style= {{height: '150px'}}  />
@@ -37,15 +35,16 @@ const CountryChar = ({ country }) => {
       <h1>Weather in {country.capital[0]}</h1>
 
       <p>
-        Temperature {temperature}
+        Temperature {weather.main ? <span>{weather.main.temp}</span>: null } °C
       </p>
 
       <p>
-        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={`Weather in ${country.capital[0]} `} />
+        {weather.weather ? <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={`Weather in ${country.capital[0]} `} /> : null} <br/>
+        {weather.weather ? <span>{weather.weather[0].description}</span> : null}
       </p>
       <p>
-        Wind {wind} m/s
-      </p>
+        Wind {weather.wind ? <span>{weather.wind.speed}</span> : null } m/s
+      </p> 
       
 
     </div>
